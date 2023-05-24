a90d63469070260c8bac869af439f91e91f67030
RajMazing <caseyraj92@gmail.com>
2023-05-23 19:45:12 -0500

    created an API that displays sayHello and sayGoodBye on their respective endpoints on the servere

---
+++ src/main/java/com/workshop/Security/GreetingsController.java
@@ -0,0 +1,24 @@
+package com.workshop.Security;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//creates server endpoint
+//Server displays sayHello()'s String on page
+@RestController
+@RequestMapping("/api/v1/greetings")
+public class GreetingsController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello from our API");
+    }
+    //Creates a server endpoint
+    // displays sayGoodBye() string on the page
+    @GetMapping("/say-good-bye")
+    public ResponseEntity<String> sayGoodBye() {
+        return ResponseEntity.ok("Good bye and see you later");
+    }
+}
