570da9239b917aed3d5170c1dfd26573bdc3b339
RajMazing <caseyraj92@gmail.com>
2023-05-29 11:11:32 -0500

    Implement JWT utils class and finalise the filter

--- src/main/java/com/workshop/Security/config/SecurityConfig.java
+++ src/main/java/com/workshop/Security/config/SecurityConfig.java
@@ -1,25 +1,30 @@
 package com.workshop.Security.config;
 
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthFilter jwtAuthFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-       http
+
+        http
                .authorizeRequests()
                .anyRequest()
                .authenticated()
                .and()
-               .httpBasic();
+               .addFilterBefore(jwtAuthFilter,  UsernamePasswordAuthenticationFilter.class);
        return http.build();
     }
 }
